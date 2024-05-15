eb33480ce43a4f5a7a07b63f001d93dff9459136
Nils0009 <143409567+Nils0009@users.noreply.github.com>
2024-05-15 11:26:59 +0200

    EmailProvider created with EmailSender as function and services + models

---
+++ EmailProvider/Models/EmailRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailProvider.Models;
+
+public class EmailRequest
+{
+    public string To { get; set; } = null!;
+    public string Subject { get; set; } = null!;
+    public string HtmlContent { get; set; } = null!;
+    public string TextContent { get; set; } = null!;
+
+}
---
+++ EmailProvider/Services/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Communication.Email;
+using Azure.Messaging.ServiceBus;
+using Azure;
+using EmailProvider.Models;
+using Newtonsoft.Json;
+using EmailProvider.Functions;
+using Microsoft.Extensions.Logging;
+namespace EmailProvider.Services;
+
+public class EmailService(ILogger<EmailSender> logger, EmailClient emailClient) : IEmailService
+{
+    private readonly ILogger<EmailSender> _logger = logger;
+    private readonly EmailClient _emailClient = emailClient;
+
+    public EmailRequest UnpackEmailRequest(ServiceBusReceivedMessage message)
+    {
+        try
+        {
+            var emailRequest = JsonConvert.DeserializeObject<EmailRequest>(message.Body.ToString());
+            if (emailRequest != null)
+                return emailRequest;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : EmailSender.UnpackEmailRequest :: {ex.Message}");
+        }
+        return null!;
+    }
+
+    public bool SendEmail(EmailRequest emailRequest)
+    {
+        try
+        {
+            var result = _emailClient.Send(
+
+            WaitUntil.Completed,
+            senderAddress: Environment.GetEnvironmentVariable("senderAddress"),
+            recipientAddress: emailRequest.To,
+            subject: emailRequest.Subject,
+            htmlContent: emailRequest.HtmlContent,
+            plainTextContent: emailRequest.TextContent);
+
+            if (result.HasCompleted)
+                return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : EmailSender.SendEmail :: {ex.Message}");
+        }
+        return false;
+    }
+}
